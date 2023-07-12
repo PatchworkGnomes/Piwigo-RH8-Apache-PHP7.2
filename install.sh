6b048464e09617783a864a97dd681e669ac492b8
Patchwork.Gnomes@outlook.com <Patchwork.Gnomes@outlook.com>
2023-07-12 07:35:02 -0400

    Update install.sh

---
+++ install.sh
@@ -0,0 +1,288 @@
+#!/bin/sh
+clear
+
+echo "===================================================================================================="
+echo ""
+echo "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWNXKKKKKXNNWMMMMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWNXK0kkxdddddddxkO0XWMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWWWWWWWWWWWWWWWWWWWWWWWWWNNXKK0OkxxddddddddodddddddxOKNWMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMMMMMMMMWNXK0OOOkkkkkkkkkkkkkkkkkkkkkkkkxxxddddddodddddddddddddoddddddOXWMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMMMMMWX0OxdddoddddddddddddddddddddddddddddddddddddddodddddxxxxddoddddddkXWMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMMWN0kxdooddddddddddddddddddddddddddddddddddddddddddddddOKNNNNKOddddddddONMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMWN0xdddodoodddddddddddddddddddddddddddddddddddddddddddd0WMMMMMMNOdddddddxKMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMWKkdddodddddddddddddddddddddddddoddddddddddddddddddooddxKWMMMMMMW0dddddddxKWMMMMMMMMMM"
+echo "MMMMMMMMMMMMMWKxdddddddddddddddddddddddddddddddddddddddddddddddddddoddkXWMMMMWKkdddddddxKWMMMMMMMMMM"
+echo "MMMMMMMMMMMMWKxddddddddddddddddddddddddddddddddddddddddddddddddddddddddxO0KK0kxdoddddddxKMMMMMMMMMMM"
+echo "MMMMMMMMMMMMXkdddddddddddddddddddddddddddddddxxkkkkkkxxxdddddddddddddddddddddddddddddddxKMMMMMMMMMMM"
+echo "MMMMMMMMMMMWKxdodddddddddddddddddddoddddkO0KXNNWWWWWWNNNXK0OxddddddddddddddodddddddddddxKWMMMMMMMMMM"
+echo "MMMMMMMMMMMW0dddddddddddddddddddddddxk0XNWMMMMMMMMMMMMMMMMMWNX0kddddddoddddddddddddddddxKWMMMMMMMMMM"
+echo "MMMMMMMMMMMNOdddddddddddddddddddddx0XWMMMMMMMMWWNNNNNWWMMMMMMMMWXOxddddddodddddddddddddxKWMMMMMMMMMM"
+echo "MMMMMMMMMMMNOddodddddddddoodddddx0NWMMMMMWXOxdlc:::::codk0NWMMMMMWXOxddddddddddddddddddxKWMMMMMMMMMM"
+echo "MMMMMMMMMMMNOdddddddddddddoddodOXWMMMMWXOo:'.............,:d0NMMMMMWXkdddddddddooddddddxKWMMMMMMMMMM"
+echo "MMMMMMMMMMMNOddddddddddddddddx0NMMMMMXx:'...................,ckNMMMMWNOddddddddddddddddxKWMMMMMMMMMM"
+echo "MMMMMMMMMMMNOdddddddddddddddd0NMMMMWOc'.......................'lKWMMMMNOddddddddoddddddxKWMMMMMMMMMM"
+echo "MMMMMMMMMMMNOdddddddddddddddONMMMMWO;..........................'c0MMMMMXkddodddddddddddxKWMMMMMMMMMM"
+echo "MMMMMMMMMMMNOddddddddddddooxKWMMMM0:............................'lXMMMMW0xdodddddddddddxKWMMMMMMMMMM"
+echo "MMMMMMMMMMMNOddddddddddddddkNMMMMNd'.............................,kWMMMMXxddoddddddddddxKWMMMMMMMMMM"
+echo "MMMMMMMMMMMNOddddddddddddddOWMMMMXl..............................'dWMMMMXkdddddddddddddxKWMMMMMMMMMM"
+echo "MMMMMMMMMMMNOddddddddddddddOWMMMMXl..............................'dWMMMMXkdddddddddddddxKWMMMMMMMMMM"
+echo "MMMMMMMMMMMNOdddddooddddoddkNMMMMNd'.............................,kWMMMMXxdddddddddddddxKWMMMMMMMMMM"
+echo "MMMMMMMMMMMNOdddddddddddddoxKWMMMM0:............................'lXMMMMWKxdddddddddddddxKWMMMMMMMMMM"
+echo "MMMMMMMMMMMNOdddddddddddddddONMMMMWO;..........................':0MMMMWXkddddddddddddddxKWMMMMMMMMMM"
+echo "MMMMMMMMMMMNOdddddddddodddddd0WMMMMWO:'.......................'lKWMMMMNOdddddddddddddddxKWMMMMMMMMMM"
+echo "MMMMMMMMMMMNOdddddddddddddddox0WMMMMWXx:'...................'ckNMMMMMNOddddddddddddddddxKWMMMMMMMMMM"
+echo "MMMMMMMMMMMNOddddddddddddddddddOXWMMMMWXkl;'.............,:oONMMMMMWXkdddddddddddddddddxKWMMMMMMMMMM"
+echo "MMMMMMMMMMMNOdddddddddddddddddddx0NWMMMMMWXOxdlc:::::cldk0NWMMMMMWXOxddddddddddddddddddxKWMMMMMMMMMM"
+echo "MMMMMMMMMMMNOdddddddddddddddddddddk0XWMMMMMMMMWNNNNNNWWMMMMMMMMWX0xddddddodddddddddddddxKWMMMMMMMMMM"
+echo "MMMMMMMMMMMW0dddddddddddddddddddddddxOKXWWMMMMMMMMMMMMMMMMMWNX0kxddddddddddddddddddddddxKWMMMMMMMMMM"
+echo "MMMMMMMMMMMWKxdddddddddddddddddddddddddxkO0KXNNWWWWWWNNNXK0OxddoooodddddddddddddodddddoxXMMMMMMMMMMM"
+echo "MMMMMMMMMMMMXkdddddddddddddddddddddddddddddddxxkkkkkkkxxddddddddoddddddddddddddddddddddONMMMMMMMMMMM"
+echo "MMMMMMMMMMMMWKxdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddkXMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMWKxdddddddddddddddddddddddddddoodddddddddddoodddddddddddddddddddddddddddkXWMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMWKkdddoddddddddddddddddddddddddddddddddddddddddddddddddddddddoddddddddOXWMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMWN0xdodddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddkKNMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMMWN0kddddddddddddddddddddddddddddddddddddddddddddddddddddddddddxkKNWMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMMMMMWX0kxdddddddddddddddddddddddddddddddddddddddddddddddddddxO0XWMMMMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMMMMMMMMWNXK00OkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkOO00KXNWMMMMMMMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+echo ""
+echo "===================================================================================================="
+
+# Hello
+echo " Attempting to install Piwigo on RH 8 and Apache "
+echo " ================================================ "
+sleep 2
+
+###############
+## Variables ##
+###############
+
+# You MUST set the Name here:
+# This is NOT the Server Name
+# This is the name that you will enter in the URL
+SERVERNAME=piwigo.Your.Domain
+
+# This is the name of the database that will get created for Piwigo
+DBNAME=piwigodb
+
+# This is the username that is needed for the Piwigo database. NOTE: This is not a Piwigo user account
+USER=piwigouser
+
+# Email for Piwigo Admin account
+# Doesn't have to be real, but recommended
+APPMAIL=your@email.com
+
+# Email for CertBot 
+# Doesn't have to be real, but recommended
+CERTMAIL=your@email.com
+
+# Color Scheme Fun
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+# This generates a random password for the root account of mariadb. You can change it if you wish
+date +%s | sha256sum | base64 | head -c 16 > /tmp/.MARIADBPASSWORD
+sleep 1 # We need to sleep 1 to change the password, otherwise we end up with the same hash
+
+# This generates a random password for the nextcloud user account in mariadb. You can change it if you wish
+date +%s | sha256sum | base64 | head -c 16 >> /tmp/.APPPASSWORD
+sleep 1 # We need to sleep 1 to change the password, otherwise we end up with the same hash
+
+
+##########################################################################
+############## NO CHANGES FROM HERE DOWN SHOULD BE REQUIRED ##############
+##########################################################################
+#Start in /TMP
+cd /tmp
+
+# Configure Firewall
+echo -e "${green}Set Firewall port 80${reset}"
+firewall-cmd --permanent --zone=public --add-service=http
+echo -e "${green}Set Firewall port 443${reset}"
+firewall-cmd --permanent --zone=public --add-service=https
+echo -e "${green}Firewall Reload${reset}"
+firewall-cmd --reload
+clear 
+
+# # Install Required Packages
+echo -e "${green}# Install Required Packages${reset}"
+dnf -y install httpd mariadb-server php php-common php-mysqlnd php-gd php-xml php-json php-fpm
+dnf update -y
+
+# Start and Enable services
+echo -e "${green}Start and Enable HTTPD services${reset}"
+systemctl enable --now httpd
+echo -e "${green}Start and Enable MariaDB services${reset}"
+systemctl enable --now mariadb
+
+# Enable Extra Repo for CertBot
+echo -e "${green}Enable Extra Repo for CertBot${reset}"
+wget -O /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-8 https://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-8
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-8
+dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm 
+dnf install certbot python3-certbot-apache -y
+echo -e "${green}Done${reset}"
+
+# Create a Genaric Virtual host
+echo -e "${green}Create a Genaric Virtual host${reset}"
+cat << EOF >> /etc/httpd/conf.d/piwigo.conf
+<VirtualHost *:80>
+  ServerAdmin admin@mydomainname.com  
+  DocumentRoot /var/www/html/piwigo
+  ServerName $SERVERNAME
+  # ServerAlias www.your_domain
+  ErrorLog "/var/log/httpd/piwigo_error_log"
+  CustomLog "/var/log/httpd/piwigo_access_log" combined
+
+  <directory /var/www/html/piwigo>
+    Require all granted
+    AllowOverride All
+    Options FollowSymLinks MultiViews
+    SetEnv HOME /var/www/html/piwigo
+    SetEnv HTTP_HOME /var/www/html/piwigo
+  </directory>
+
+</VirtualHost>
+EOF
+
+#Restart httpd 
+echo -e "${green}Restart Apache${reset}"
+systemctl restart httpd.service
+
+# Setup SQL
+echo -e "${green}Setting up DB\User\Password for Piwigo${reset}"
+cat << EOF >> /tmp/setup.sql
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$(cat /tmp/.MARIADBPASSWORD)');
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
+CREATE DATABASE $DBNAME DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE USER $USER@localhost IDENTIFIED BY '$(cat /tmp/.APPPASSWORD)';
+GRANT ALL PRIVILEGES ON $DBNAME.* TO $USER@localhost;
+FLUSH PRIVILEGES;
+quit
+EOF
+
+mysql -u root < /tmp/setup.sql
+
+# Download and install Piwigo
+echo -e "${green}Download and install Piwigo${reset}"
+cd /var/www/html/
+wget wget https://piwigo.org/download/dlcounter.php?code=latest
+mv dlcounter.php?code=latest piwigo.zip
+unzip piwigo.zip
+mv piwigo.* piwigo 
+
+# Selinux 
+echo -e "${green}Setting Selinux for Piwigo${reset}"
+chcon -R -t httpd_sys_content_t /var/www/html/piwigo
+semanage fcontext -a -t httpd_sys_content_t "/var/www/html/piwigo(/.*)?"
+restorecon -R -v /var/www/html/piwigo
+setsebool -P httpd_can_network_connect 1
+setsebool -P httpd_unified 1
+
+# Set permisions
+echo -e "${green}Setting Permissions for Piwigo${reset}"
+chmod go+x /var/www/html/piwigo
+chown -R apache:apache /var/www/html/piwigo
+chmod -R 755 /var/www/html/piwigo
+chmod -R 777 /var/www/html/piwigo/_data
+
+# Setting up for failed logins for fail2ban
+echo -e "${green}Installing fail2ban${reset}"
+dnf install fail2ban -y
+
+echo -e "${green}Create Jail for Piwigo${reset}"
+cat << EOF >> /etc/fail2ban/jail.local
+[piwigo]
+enabled = true
+port = http,https
+filter = piwigo
+logpath = /var/log/httpd/piwigoFailedLogins.log
+EOF
+
+echo -e "${green}Create Filter for Piwigo${reset}"
+cat << EOF >> /etc/fail2ban/filter.d/piwigo.conf
+[INCLUDES]
+before = common.conf
+[Definition]
+failregex = ip=<HOST>
+ignoreregex =
+EOF
+
+# Creating log for fail2ban
+echo -e "${green}Creating log for fail2ban${reset}"
+touch /var/log/httpd/piwigoFailedLogins.log
+
+# Setting Permissions
+echo -e "${green}Setting Permissions${reset}"
+chown apache:apache /var/log/httpd/piwigoFailedLogins.log
+chown root:apache /var/log/httpd/
+chmod 660 /var/log/httpd/piwigoFailedLogins.log
+chmod 750 /var/log/httpd/
+
+# Settinga SElinux Context
+# Change file context
+echo -e "${green}Change file context${reset}"
+semanage fcontext -a -t httpd_log_t "/var/log/httpd/piwigoFailedLogins.log"
+restorecon /var/log/httpd/piwigoFailedLogins.log
+
+# Change directory context
+echo -e "${green}Change directory context${reset}"
+semanage fcontext -a -t httpd_log_t "/var/log/httpd(/.*)?"
+restorecon -R /var/log/httpd/
+
+# Edit /etc/php.ini
+echo -e "${green}Making a backup of php.ini just in case${reset}"
+cp /etc/php.ini /etc/php.ORIGINAL
+echo -e "${green}Making changes to the php.ini file${reset}"
+sed -i 's/memory_limit = 128M/memory_limit = 1024M/' /etc/php.ini
+sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 1024M/' /etc/php.ini
+sed -i 's/post_max_size = 8M/post_max_size = 1024M/' /etc/php.ini
+sed -i 's/max_execution_time = 30/max_execution_time = 360/' /etc/php.ini
+sed -i 's/;date.timezone =/date.timezone = America\/New_York/' /etc/php.ini
+
+# Restart Services
+systemctl restart httpd
+systemctl enable --now fail2ban
+clear
+
+# Manual work
+echo -e "${red}To Finish Install ${reset}"
+echo -e "${red}=====================${reset}"
+echo -e "${green}Open an internet browser${reset}"
+echo -e "${green}HTTP://<HOST-IP>${reset}"
+echo ""
+echo -e "${green}DataBase Name: $DBNAME${reset}"
+echo -e "${green}DB User: $USER${reset}"
+echo -e "${green}Password:" $(cat /tmp/.APPPASSWORD) "${reset}"
+echo -e "${green}Admin Email: $APPMAIL${reset}"
+echo ""
+echo ""
+echo -e "${red}Don't Forget to Clean the TMP${reset}"
+echo -e "${red}.APPPASSWORD${reset}"
+echo -e "${red}.MARIADBPASSWORD${reset}"
+echo -e "${red}setup.sql${reset}"
+
+
+# certbot --apache --agree-tos --redirect --uir --hsts --staple-ocsp --must-staple -d $SERVERNAME --email $CERTMAIL
+
+
+# Disable reading by anonymous users
+#$wgGroupPermissions['*']['read'] = false;
+
+
+
+
+
+
